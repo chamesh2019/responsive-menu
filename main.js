aa18928477ba8f13984c643082092179911ea0a2
Chamesh Dinuka <101823341+chamesh2019@users.noreply.github.com>
2023-03-12 14:06:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 
+const links = document.getElementsByClassName("link");
+
+for (const link of links){
+  link.onmousemove = e => {
+    const decimal = e.clientX / link.offsetWidth;
+    const basepresent = 60,
+          range = 40,
+          ajustable = range * decimal;
+    const finalpresentage = `${basepresent + ajustable}%`;
+    link.style.setProperty("--p", finalpresentage);
+  }
+}
